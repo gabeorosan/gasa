6ca8479b234465e70aaa8c13e3d9837fdb5d5292
Kageyama Tobio <kageyamatobio@No-Camera-2020-Macbook-Air-8-GB.local>
2023-08-18 11:32:00 -0400

    better song filter

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+n_songs = 168;
 
+var apiKey
 
-n_songs = 270;
 var n = Math.floor(Math.random() * n_songs);
 var lyricsFile = 'lyrics/' + n + '.json';
 var lyrics, currentSentence, words
 var sentenceElement = document.getElementById("sentence");
 
-function nextQuestion() {
+document.getElementById('title').addEventListener('click', function() {
+  var url = "https://www.google.com/search?q=" + encodeURIComponent(this.innerText + ' lyrics');
+  window.open(url, '_blank');
+});
 
-  if (currentSentence >= lyrics.length) {
-    alert("End of lyrics!");
-    return;
+function nextQuestion() {
+  if (currentSentence >= lyrics.length - 1) {
+    currentSentence = 0;
+  } else {
+    currentSentence++;
   }
   var sentence = lyrics[currentSentence];
   var sentenceWords = sentence.split(" ");
@@ -21,8 +27,7 @@ function nextQuestion() {
 
   // set the innerText of the sentence element to the new sentence
   sentenceElement.innerText = sentence;
-  // set the progress text
-  document.getElementById("progress").innerText = "Progress: " + (currentSentence + 1) + "/" + lyrics.length;
+  
 
   var wordsCopy = words.slice();
   
@@ -46,7 +51,13 @@ function nextQuestion() {
     optionsDiv.appendChild(optionDiv);
   });
 
-  currentSentence++;
+  
+  document.getElementById("progress").innerText = (currentSentence + 1) + "/" + lyrics.length;
+}
+
+function previousQuestion() {
+  currentSentence = currentSentence === 0 ? lyrics.length - 2 : currentSentence - 2;
+  nextQuestion();
 }
 
 
@@ -54,14 +65,16 @@ function nextQuestion() {
 function checkAnswer(selected, correct, optionDiv) {
 
     if (selected === correct) {
-        optionDiv.style.backgroundColor = "green";
+        optionDiv.style.backgroundColor = "#44b445";
       } else {
         optionDiv.style.backgroundColor = "red";
         // Highlight the correct answer
-        document.getElementById("correct").style.backgroundColor = "green";
+        document.getElementById("correct").style.backgroundColor = "#44b445";
       }
         var corr = document.getElementById("correct").innerHTML
-        //sentenceElement.innerText = sentenceElement.innerText.replace("_____", corr);
+  
+        // Replace the blank with the correct answer in green
+        sentenceElement.innerHTML = sentenceElement.innerText.replace("_____", "<span style='color:green'>" + corr + "</span>");
 
       // Disable all options
       var optionsDiv = document.getElementById("options");
@@ -92,21 +105,63 @@ function newSong() {
   .then((data) => {
     lyrics = data.lyrics;
     title = data.title;
+    searchYouTube(title);
     document.getElementById("title").innerText = title;
-    currentSentence = 0;
+    currentSentence = -1;
     createWords();
     nextQuestion();
   });
 }
 
-fetch(lyricsFile)
-  .then((response) => response.json())
-  .then((data) => {
-    lyrics = data.lyrics;
-    title = data.title;
-    document.getElementById("title").innerText = title;
-    currentSentence = 0;
-    createWords();
-    nextQuestion();
-  });
+
+function searchYouTube(title) {
+  var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${title}&type=video&key=${apiKey}`;  
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      var videoId = data.items[0].id.videoId;
+      playVideo(videoId);
+    });
+}
+
+
+
+var player;
+
+function playVideo(videoId) {
+  if (!player) {
+    player = new YT.Player('player', {
+      height: '360',
+      width: '640',
+      videoId: videoId,
+    });
+  } else {
+    player.loadVideoById(videoId);
+  }
+}
+
+
+function onYouTubeIframeAPIReady() {
+  
+
+  fetch('tokens.json')
+    .then((response) => response.json())
+    .then((data) => {
+      apiKey = data.apiKey;
+    })
+
+    fetch(lyricsFile)
+    .then((response) => response.json())
+    .then((data) => {
+      lyrics = data.lyrics;
+      title = data.title;
+      searchYouTube(title);
+      document.getElementById("title").innerText = title;
+      currentSentence = -1;
+      createWords();
+      nextQuestion();
+    });
+}
+
+
 
